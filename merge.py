1a719c703562dd1f4bb197f2a684f73e3b961733
Adnan Ardhian <ardhianadnan@gmail.com>
2018-03-30 19:28:24 +0700

    Update merge.py

--- merge.py
+++ merge.py
@@ -5,7 +5,7 @@ def merge(left, right):
         if left[i].split(' ')[1] < right[j].split(' ')[1]: #sort by age first
             result.append(left[i])
             i+= 1
-        elif left[i].split(' ')[1] == right[j].split(' ')[1]: #if age is the same, sort by name
+        elif left[i].split(' ')[1] == right[j].split(' ')[1]: #if the age are same, sort by name
             if left[i] < right[j]:
                 result.append(left[i])
                 i+= 1
@@ -31,12 +31,14 @@ def mergesort(list):
 
     return merge(left, right)
 
+# READ FILE
 file_object = open("PATH_TO_FILE/name_age.txt", "r")
 input_file = file_object.read()
 input_file = input_file.split('\n')
 input_file = list(map(str, input_file))
 sorted_file = mergesort(input_file)
 
+# WRITE OUTPUT FILE
 file_output = open("PATH_TO_FILE/sorted_name_age.txt", "w")
 for i in range(0,len(sorted_file)):
     file_output.write(str(sorted_file[i]))
